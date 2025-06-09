8ae0b5671bcff19a768b1fca2d27990bf5cf45c4
Valentyn Bondarenko <murica.it@gmail.com>
2025-06-09 09:23:51 +0200

    Add project files.

---
+++ TexturedSphere3d/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace TexturedSphere3d
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
